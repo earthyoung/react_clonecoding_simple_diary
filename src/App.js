cf26cc864e449e39ce92deedd0cfe23b6ced15eb
earthyoung <studydatawithme@gmail.com>
2023-11-28 16:44:43 +0900

    feat: DiaryList.js & DiaryItem.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    "id": 1,
+    "author": "soyoung",
+    "content": "hello",
+    "emotion": 1,
+    "created_date": new Date().getTime()
+  },
+  {
+    "id": 2,
+    "author": "earthyoung",
+    "content": "I WANT YOU",
+    "emotion": 4,
+    "created_date": new Date().getTime()
+  },
+  {
+    "id": 3,
+    "author": "ksy",
+    "content": "LOL",
+    "emotion": 5,
+    "created_date": new Date().getTime()
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
