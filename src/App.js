b0579aef86dbfb0ec847a11de0cfe55984857f0b
earthyoung <studydatawithme@gmail.com>
2023-11-28 17:33:33 +0900

    feat: 데이터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import {useState, useRef} from "react";
 
-const dummyList = [
-  {
-    "id": 1,
-    "author": "soyoung",
-    "content": "hello",
-    "emotion": 1,
-    "created_date": new Date().getTime()
-  },
-  {
-    "id": 2,
-    "author": "earthyoung",
-    "content": "I WANT YOU",
-    "emotion": 4,
-    "created_date": new Date().getTime()
-  },
-  {
-    "id": 3,
-    "author": "ksy",
-    "content": "LOL",
-    "emotion": 5,
-    "created_date": new Date().getTime()
-  },
-];
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current
+    };
+    dataId.current += 1
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList}/>
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList diaryList={data}/>
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import { useRef, useState} from "react";
 
-const DiaryEditor = () => {    
+const DiaryEditor = ({onCreate}) => {    
 
     const authorInput = useRef();
     const contentInput = useRef();
@@ -18,7 +18,7 @@ const DiaryEditor = () => {
         });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = () => {
         if (state.author.length < 1) {
             authorInput.current.focus();
             return;
@@ -27,7 +27,13 @@ const DiaryEditor = () => {
             contentInput.current.focus();
             return;
         }
+        onCreate(state.author, state.content, state.emotion);
         alert("저장 성공");
+        setState({
+            author: "",
+            content: "",
+            emotion: 1,
+        });
     };
 
     return <div className="DiaryEditor">
