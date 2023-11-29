91a8ec07daa281acd71df959af075a6191a40bec
earthyoung <studydatawithme@gmail.com>
2023-11-29 15:01:18 +0900

    feat: API 호출해서 일기 초기 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import {useState, useRef} from "react";
-import LifeCycle from './LifeCycle';
+import {useState, useRef, useEffect} from "react";
 
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments").then((res)=>res.json());
+    const initData = res.slice(0, 20).map((it)=>{
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    })
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -36,7 +54,6 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle />
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit}/>
     </div>
