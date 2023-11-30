8970c2853f8f03f9410cce743dc987cdc7663c5e
earthyoung <studydatawithme@gmail.com>
2023-11-30 15:22:19 +0900

    feat: useCallback -> DiaryEditor.js 최적화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import {useMemo, useState, useRef, useEffect} from "react";
+import {useMemo, useState, useRef, useEffect, useCallback} from "react";
 // import OptimizeTest from './OptimizeTest';
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
     getData();
   }, []);
 
-  const onCreate = (author, content, emotion) => {
+  const onCreate = useCallback((author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -37,8 +37,8 @@ function App() {
       id: dataId.current
     };
     dataId.current += 1
-    setData([newItem, ...data]);
-  };
+    setData((data)=>[newItem, ...data]);
+  }, []);
 
   const onRemove = (targetId) => {
     const newData = data.filter((it) => it.id !== targetId);
